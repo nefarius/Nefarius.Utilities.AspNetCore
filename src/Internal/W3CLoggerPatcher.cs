5104300ea705079504de988e49afac4c799c2797
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-04-29 17:45:19 +0200

    Got hooking stuff to work

--- src/Internal/W3CLoggerPatcher.cs
+++ src/Internal/W3CLoggerPatcher.cs
@@ -1,28 +1,52 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
-using HarmonyLib;
+using MonoMod.RuntimeDetour;
 
 namespace Nefarius.Utilities.AspNetCore.Internal;
 
+/// <summary>
+///     Alters the file rolling behaviour of the W3CLoggerProcessor.
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
 internal sealed class W3CLoggerPatcher
 {
+    private static IDetour _hookRollFiles;
+    private static readonly Type FileLoggerProcessor;
+
+    static W3CLoggerPatcher()
+    {
+        FileLoggerProcessor = GetTypeByName("Microsoft.AspNetCore.HttpLogging.FileLoggerProcessor");
+    }
+
+    /// <summary>
+    ///     Patch desired methods.
+    /// </summary>
     public static void Patch()
     {
-        Harmony.DEBUG = true;
-        
-        var harmony = new Harmony("Microsoft.AspNetCore.HttpLogging.FileLoggerProcessor");
-        
-        var fileLoggerProcessor = ByName("Microsoft.AspNetCore.HttpLogging.FileLoggerProcessor");
-        var original = AccessTools.Method(fileLoggerProcessor, "RollFiles");
-        var prefix = typeof(W3CLoggerPatcher).GetMethod("RollFilesPrefix");
-        var postfix = typeof(W3CLoggerPatcher).GetMethod("RollFilesPostfix");
-        
-        harmony.Patch(original, new HarmonyMethod(prefix), new HarmonyMethod(postfix));
+        _hookRollFiles = new Hook(
+            FileLoggerProcessor.GetMethod("RollFiles", BindingFlags.Instance | BindingFlags.NonPublic),
+            typeof(W3CLoggerPatcher).GetMethod("RollFiles", BindingFlags.Static | BindingFlags.NonPublic)
+        );
     }
-    
-    private static Type ByName(string name)
+
+    /// <summary>
+    ///     Restore patches to their original states.
+    /// </summary>
+    public static void Unpatch()
+    {
+        _hookRollFiles.Undo();
+    }
+
+    /// <summary>
+    ///     Find a type by fully qualified name in all assemblies.
+    /// </summary>
+    /// <param name="name">The name.</param>
+    /// <returns>The type.</returns>
+    private static Type GetTypeByName(string name)
     {
         return
             AppDomain.CurrentDomain.GetAssemblies()
@@ -38,15 +62,23 @@ internal sealed class W3CLoggerPatcher
                 .FirstOrDefault(t => t.Name.Contains(name));
     }
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public static void RollFilesPrefix()
+    private static object GetInstanceField(Type type, object instance, string fieldName)
     {
-        // ...
+        BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                                 | BindingFlags.Static;
+        FieldInfo field = type.GetField(fieldName, bindFlags);
+        return field?.GetValue(instance);
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    public static void RollFilesPostfix()
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    private static void RollFiles(dynamic fileLoggerProcessor)
     {
+        string path = GetInstanceField(FileLoggerProcessor, fileLoggerProcessor, "_path");
+        string fileName = GetInstanceField(FileLoggerProcessor, fileLoggerProcessor, "_fileName");
+        object pathLock = GetInstanceField(FileLoggerProcessor, fileLoggerProcessor, "_pathLock");
+        int? maxRetainedFiles = GetInstanceField(FileLoggerProcessor, fileLoggerProcessor, "_maxRetainedFiles");
+
         // ...
     }
 }
