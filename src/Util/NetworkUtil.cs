2b07510475ee6a0aff0bc5ae0e661ece0a8bbcaf
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-11-08 22:11:30 +0100

    Fixing System.Net.Sockets.SocketException (95): Operation not supported

--- src/Util/NetworkUtil.cs
+++ src/Util/NetworkUtil.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
-
 #if !NET8_0_OR_GREATER
 // Tuple substitute, since we only care about the values
 using IPNetwork = (System.Net.IPAddress BaseAddress, int PrefixLength);
@@ -12,51 +12,68 @@ using IPNetwork = (System.Net.IPAddress BaseAddress, int PrefixLength);
 
 namespace Nefarius.Utilities.AspNetCore.Util;
 
+[SuppressMessage("ReSharper", "InconsistentNaming")]
 internal static class NetworkUtil
 {
     public static IEnumerable<IPNetwork> GetNetworks()
-        => IPGlobalProperties
+    {
+        return IPGlobalProperties
             .GetIPGlobalProperties()
             .GetUnicastAddresses()
             .Select(GetIPNetwork);
+    }
 
     /// <summary>
-    /// Computes the network prefix address from the IP and prefix length as per CIDR
+    ///     Computes the network prefix address from the IP and prefix length as per CIDR
     /// </summary>
     /// <param name="info">The IP info</param>
     /// <returns>The IP address representing the network prefix</returns>
-    public static IPAddress GetIPNetworkPrefix(this UnicastIPAddressInformation info)
+    private static IPAddress GetIPNetworkPrefix(this UnicastIPAddressInformation info)
     {
         if (info.PrefixLength == 0)
+        {
             return info.Address.AddressFamily == AddressFamily.InterNetwork ? IPAddress.Any : IPAddress.IPv6Any;
-        var maxPrefix = info.Address.AddressFamily == AddressFamily.InterNetwork ? 32 : 128;
-        if (info.PrefixLength == maxPrefix)
-            return info.Address;
-        if (info.PrefixLength > maxPrefix)
-            throw new ArgumentOutOfRangeException("info.PrefixLength");
+        }
 
-        var bytes = info.Address.GetAddressBytes();
-        var bitsToBeZeroed = maxPrefix - info.PrefixLength;
+        int maxPrefix = info.Address.AddressFamily == AddressFamily.InterNetwork ? 32 : 128;
+        if (info.PrefixLength == maxPrefix)
+        {
+            return info.Address;
+        }
+
+        if (info.PrefixLength > maxPrefix)
+        {
+            throw new ArgumentOutOfRangeException($"{nameof(info)}.PrefixLength");
+        }
+
+        byte[] bytes = info.Address.GetAddressBytes();
+        int bitsToBeZeroed = maxPrefix - info.PrefixLength;
 
         // Big-endian, so we zero bits right to left
-        var i = bytes.Length;
-        while (i --> 0 && bitsToBeZeroed >= 8)
+        int i = bytes.Length;
+        while (i-- > 0 && bitsToBeZeroed >= 8)
         {
             bytes[i] = 0;
             bitsToBeZeroed -= 8;
         }
+
         if (bitsToBeZeroed > 0)
         {
             bytes[i] &= (byte)(byte.MaxValue << bitsToBeZeroed);
         }
-        return new IPAddress(bytes, info.Address.ScopeId);
+
+        return info.Address.AddressFamily == AddressFamily.InterNetwork
+            ? new IPAddress(bytes)
+            : new IPAddress(bytes, info.Address.ScopeId);
     }
 
     /// <summary>
-    /// Computes the IPNetwork that the IP address belongs to
+    ///     Computes the IPNetwork that the IP address belongs to
     /// </summary>
     /// <param name="info">The IP info</param>
     /// <returns>The IPNetwork that th# IP belongs to</returns>
-    public static IPNetwork GetIPNetwork(this UnicastIPAddressInformation info)
-        => new(info.GetIPNetworkPrefix(), info.PrefixLength);
+    private static IPNetwork GetIPNetwork(this UnicastIPAddressInformation info)
+    {
+        return new IPNetwork(info.GetIPNetworkPrefix(), info.PrefixLength);
+    }
 }
